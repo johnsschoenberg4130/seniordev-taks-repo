c176cf4a6d67397441b7e3fda0c79fbf8c1e6a44
johnsschoenberg4130 <106274974+johnsschoenberg4130@users.noreply.github.com>
2023-01-20 16:58:50 -0700

    able to connect to postman and grab empty object from post

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const fs = require('fs/promises')
 const express = require('express');
 // const cors = require('cors');
 // const lodash = require('loadash');
+const { v4: uuid} = require('uuid');
 
 
 const app = express(); 
 
+app.use(express.json()) // middle wear for json
+
+// test to determine my port is on .. 
+app.get("/message",(req, res) =>{
+    res.send('This is working....')
+})
+
 // ping that request once we are able to connect 
- app.get('/message', (req, res) =>{
-    res.send('Testing here....');
+ app.post("/getinfo", (req, res) => {
+    // various required 
+    const id = uuid();
+    const content = req.body;
+    console.log(id); // test for post 
+    
+    console.log(content)
+    res.sendStatus(201);
+
  })
 
 // define the rest end point 
